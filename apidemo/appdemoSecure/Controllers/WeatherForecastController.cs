e09a0c03f067621535b52cb6e76a3f1680101379
Uffe Hansen <uffe.hansen@gmail.com>
2021-05-14 14:45:47 +0200

    Tilføjet et secure projekt

---
+++ apidemo/appdemoSecure/Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+﻿using apidemo.core.models;
+using apidemo.core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace appdemoSecure.Controllers
+{
+    [Authorize]
+    public class WeatherForecastController : Controller
+    {
+        IWeatherForecastService _service;
+        public WeatherForecastController(IWeatherForecastService service)
+        {
+            _service = service;
+        }
+        public IActionResult Index() => View(_service.GetForeCastSeries());
+        
+
+        
+        public IActionResult Create(WeatherForecast forecast)
+        {        
+
+            return View(nameof(Create));
+        }
+
+        [HttpPost(Name ="Create")]
+        public IActionResult CreateNew(WeatherForecast forecast)
+        {
+            _service.Add(forecast);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
